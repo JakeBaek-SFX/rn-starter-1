b8b781303db570daf7ddb28b9140da5beb375c3f
Jake Baek <jake.baek@soulfx.com>
2024-01-29 23:31:51 -0500

    fix: added FlatList

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -3,23 +3,28 @@ import { Text, FlatList, StyleSheet } from 'react-native';
 
 const ListScreen = () => {
   const itemList = [
-    {name: 'Friend #1', key: '1'},
-    {name: 'Friend #2', key: '2'},
-    {name: 'Friend #3', key: '3'},
-    {name: 'Friend #4', key: '4'},
-    {name: 'Friend #5', key: '5'},
+    {name: 'Friend #1', key: '1', age: 20},
+    {name: 'Friend #2', key: '2', age: 45},
+    {name: 'Friend #3', key: '3', age: 32},
+    {name: 'Friend #4', key: '4', age: 27},
+    {name: 'Friend #5', key: '5', age: 53},
+    {name: 'Friend #7', key: '6', age: 30},
   ]
   return (
     <FlatList 
       keyExtractor={(item) => item.key}
       data={itemList} 
       renderItem={({item}) => {
-        return <Text>{item.name}</Text>
+        return <Text style={styles.textStyle}>{item.name} - Age {item.age}</Text>
       }} 
     />
   )
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  textStyle:{
+    marginVertical: 10
+  }
+});
 
 export default ListScreen;
